b4559a9a8a6f1df362f72dfc987239ee040c0a05
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2025-11-08 18:13:21 -0500

    avatar faces direction, rotates smoothly, and animates

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,15 @@ extends CharacterBody3D
 @export var move_speed:= 8.0
 @export var acceleration := 20.0
 
+var last_movement_direction := Vector3.BACK
+
 @onready var hallway_camera: Camera3D = $"../hallway_camera"
 @onready var vault_camera: Camera3D = $"../vault_camera"
+@onready var player_skin: Node3D = %player_skin
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 var active_camera: Camera3D = hallway_camera
+var rotation_speed := 12.0
 
 func _ready() -> void:
 	hallway_camera.make_current()
@@ -24,6 +29,20 @@ func _physics_process(delta: float) -> void:
 
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
 	move_and_slide()
+
+	if move_direction.length() > 0.2:
+		last_movement_direction = move_direction
+		# TODO: Turn this into a state machine.
+		if animation_player.current_animation != "walking":
+			animation_player.play("walking")
+	else:
+		# TODO: Turn this into a state machine.
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
+
+	var target_angle := Vector3.BACK.signed_angle_to(last_movement_direction, Vector3.UP)
+
+	player_skin.global_rotation.y = lerp_angle(player_skin.global_rotation.y, target_angle, rotation_speed * delta)
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
