3630d1c72641d4f11b430a902746999ce40d1206
echoatday <echoatday@gmail.com>
2025-11-16 17:19:51 -0500

    slasher refactored for Input.get_vector

--- scripts/slasher.gd
+++ scripts/slasher.gd
@@ -1,18 +1,22 @@
 extends Node
 
-const max_counter = 10
+var max_counter
 var counter
-var direction
-var last_direction
+var direction: Vector2
+var current_hit: Vector2
+var last_hit: Vector2
+var slash: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	max_counter = 10
 	counter = max_counter
-	direction = "center"
+	current_hit = direction
+	last_hit = direction
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if counter == 0:
 		$hoz.visible = false
 		$vert.visible = false
@@ -20,74 +24,58 @@ func _process(delta: float) -> void:
 		$botright.visible = false
 		$topleft.visible = false
 		$topright.visible = false
+		current_hit = Vector2(0,0)
+		last_hit = Vector2(0,0)
 		counter = max_counter
 	elif counter < max_counter:
 		counter -= 1
 	
-	if Input.is_action_just_pressed("slash_left"):
-		last_direction = direction
-		direction = "left"
-		direction_calculation()
-		counter = max_counter - 1
-	if Input.is_action_just_pressed("slash_right"):
-		last_direction = direction
-		direction = "right"
-		direction_calculation()
-		counter = max_counter - 1
-	if Input.is_action_just_pressed("slash_up"):
-		last_direction = direction
-		direction = "up"
-		direction_calculation()
-		counter = max_counter - 1
-	if Input.is_action_just_pressed("slash_down"):
-		last_direction = direction
-		direction = "down"
-		direction_calculation()
-		counter = max_counter - 1
+	if counter == max_counter:
+		direction = floor(Input.get_vector("slash_left", "slash_right", "slash_up", "slash_down"))
+	else:
+		direction = Vector2(0,0)
+		
+	if Input.is_action_pressed("block"):
+		$hoz.billboard = true
+		$vert.billboard = true
+		$botleft.billboard = true
+		$botright.billboard = true
+		$topleft.billboard = true
+		$topright.billboard = true
+		direction = floor(Input.get_vector("slash_left", "slash_right", "slash_up", "slash_down"))
+		counter = 1
+	else:
+		$hoz.billboard = false
+		$vert.billboard = false
+		$botleft.billboard = false
+		$botright.billboard = false
+		$topleft.billboard = false
+		$topright.billboard = false
+	
+	if direction != current_hit and direction != Vector2(0,0):
+		last_hit = current_hit
+		current_hit = direction
+		if last_hit + current_hit != Vector2(0,0): # vert/hoz slashes register 0,0 always
+			slash = last_hit + current_hit
+		else:
+			slash = current_hit
+		counter = max_counter-1
+		slash_display()
+		#print("last hit: "+str(last_hit))
+		#print("current hit: "+str(current_hit))
+		#print("total: "+str(slash))
 
-func direction_calculation() -> void:
-	$hoz.visible = false
-	$vert.visible = false
-	$botleft.visible = false
-	$botright.visible = false
-	$topleft.visible = false
-	$topright.visible = false
-	match last_direction:
-		"left":
-			match direction:
-				"left", "right": 
-					$hoz.visible = true
-				"up":
-					$topleft.visible = true
-				"down":
-					$botleft.visible = true
-		"right":
-			match direction:
-				"left", "right": 
-					$hoz.visible = true
-				"up":
-					$topright.visible = true
-				"down":
-					$botright.visible = true
-		"up":
-			match direction:
-				"up", "down": 
-					$vert.visible = true
-				"left":
-					$topleft.visible = true
-				"right":
-					$topright.visible = true
-		"down":
-			match direction:
-				"up", "down": 
-					$vert.visible = true
-				"left":
-					$botleft.visible = true
-				"right":
-					$botright.visible = true
-		"center":
-			match direction:
-				"up", "down": 
-					$vert.visible = true
-				"left", "right":
-					$hoz.visible = true
+func slash_display() -> void:
+	match slash:
+		Vector2(1.0,0), Vector2(-1.0,0):
+			$hoz.visible = true
+		Vector2(0,1.0), Vector2(0,-1.0):
+			$vert.visible = true
+		Vector2(-1,-1):
+			$topleft.visible = true
+		Vector2(1,-1):
+			$topright.visible = true
+		Vector2(1,1):
+			$botright.visible = true
+		Vector2(-1,1):
+			$botleft.visible = true
