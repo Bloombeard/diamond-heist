8217c3affac6ff56c530938dbf00043178052e0f
echoatday <echoatday@gmail.com>
2025-11-18 19:55:57 -0500

    slasher processes patterns. has a little 3d as a treat

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,7 @@ var last_movement_direction := Vector3.BACK
 @onready var vault_camera: Camera3D = $"../vault_camera"
 @onready var player_skin: Node3D = %player_skin
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var slasher := $player_skin/Slasher
 
 var active_camera: Camera3D = hallway_camera
 var rotation_speed := 12.0
@@ -28,9 +29,8 @@ func _physics_process(delta: float) -> void:
 	move_direction = move_direction.normalized()
 
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
-	if $player_skin/Slasher.counter < $player_skin/Slasher.max_counter:
+	if slasher.counter < slasher.max_counter or Input.is_action_pressed("block"):
 		velocity = velocity.move_toward(Vector3(0,0,0), acceleration * 2 * delta)
-	
 	move_and_slide()
 
 	if velocity.length() > 0.2:
--- scripts/slasher.gd
+++ scripts/slasher.gd
@@ -1,11 +1,14 @@
 extends Node
 
+@onready var slash_effect = $slash_effect
 var max_counter
 var counter
 var direction: Vector2
 var current_hit: Vector2
 var last_hit: Vector2
 var slash: Vector2
+var pattern := Dictionary()
+var pattern_result := "none"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,43 +17,33 @@ func _ready() -> void:
 	current_hit = direction
 	last_hit = direction
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if counter == 0:
+		slash_effect.visible = false
+		counter = max_counter
+	elif counter < max_counter:
+		counter -= 1
+	
+	# wait for an attack to end before allowing another attack.
+	if counter == max_counter:
+		direction = floor(Input.get_vector("slash_left", "slash_right", "slash_up", "slash_down"))
+	else:
+		direction = Vector2(0,0)
+	
+	if Input.is_action_just_released("block") or Input.is_action_just_pressed("block"):
+		if pattern.size() == 4 and pattern.has_all(["vert", "topleft", "topright", "hoz"]):
+			pattern_result = "launch"
+			print("LAUNCH")
+		pattern.clear()
+		current_hit = Vector2(0,0)
+		last_hit = Vector2(0,0)
 		$hoz.visible = false
 		$vert.visible = false
 		$botleft.visible = false
 		$botright.visible = false
 		$topleft.visible = false
 		$topright.visible = false
-		current_hit = Vector2(0,0)
-		last_hit = Vector2(0,0)
-		counter = max_counter
-	elif counter < max_counter:
-		counter -= 1
-	
-	if counter == max_counter:
-		direction = floor(Input.get_vector("slash_left", "slash_right", "slash_up", "slash_down"))
-	else:
-		direction = Vector2(0,0)
-		
-	if Input.is_action_pressed("block"):
-		$hoz.billboard = true
-		$vert.billboard = true
-		$botleft.billboard = true
-		$botright.billboard = true
-		$topleft.billboard = true
-		$topright.billboard = true
-		direction = floor(Input.get_vector("slash_left", "slash_right", "slash_up", "slash_down"))
-		counter = 1
-	else:
-		$hoz.billboard = false
-		$vert.billboard = false
-		$botleft.billboard = false
-		$botright.billboard = false
-		$topleft.billboard = false
-		$topright.billboard = false
 	
 	if direction != current_hit and direction != Vector2(0,0):
 		last_hit = current_hit
@@ -61,21 +54,38 @@ func _physics_process(delta: float) -> void:
 			slash = current_hit
 		counter = max_counter-1
 		slash_display()
-		#print("last hit: "+str(last_hit))
-		#print("current hit: "+str(current_hit))
-		#print("total: "+str(slash))
+	slash_effect.position.z = 1 + (float(counter)/-30)
 
 func slash_display() -> void:
 	match slash:
 		Vector2(1.0,0), Vector2(-1.0,0):
-			$hoz.visible = true
+			slash_effect.rotation_degrees.x = 0
 		Vector2(0,1.0), Vector2(0,-1.0):
-			$vert.visible = true
-		Vector2(-1,-1):
-			$topleft.visible = true
-		Vector2(1,-1):
-			$topright.visible = true
-		Vector2(1,1):
-			$botright.visible = true
-		Vector2(-1,1):
-			$botleft.visible = true
+			slash_effect.rotation_degrees.x = 90
+		Vector2(-1,-1), Vector2(1,1):
+			slash_effect.rotation_degrees.x = -45
+		Vector2(1,-1), Vector2(-1,1):
+			slash_effect.rotation_degrees.x = 45
+	slash_effect.visible = true
+	
+	if Input.is_action_pressed("block"):
+		match slash:
+			Vector2(1.0,0), Vector2(-1.0,0):
+				pattern.set("vert", true)
+				$hoz.visible = true
+			Vector2(0,1.0), Vector2(0,-1.0):
+				pattern.set("hoz", true)
+				$vert.visible = true
+			Vector2(-1,-1):
+				pattern.set("topleft",true)
+				$topleft.visible = true
+			Vector2(1,-1):
+				pattern.set("topright",true)
+				$topright.visible = true
+			Vector2(1,1):
+				pattern.set("botright",true)
+				$botright.visible = true
+			Vector2(-1,1):
+				pattern.set("botleft",true)
+				$botleft.visible = true
+		
