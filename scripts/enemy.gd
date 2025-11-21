050504381cc085738a8c22220f81c0fabd67522c
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2025-11-21 15:53:45 -0500

    enemy with pathfinding added

---
+++ scripts/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+@export var player_path: NodePath
+@export var move_speed := 4.0
+
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
+
+var player = null
+
+func _ready() -> void:
+	print("ready!")
+	player = get_node(player_path)
+	
+func _physics_process(_delta: float) -> void:
+	velocity = Vector3.ZERO
+	
+	nav_agent.set_target_position(player.global_position)
+	var next_nav_point := nav_agent.get_next_path_position()
+	velocity = (next_nav_point - global_position).normalized() * move_speed
+
+	move_and_slide()
