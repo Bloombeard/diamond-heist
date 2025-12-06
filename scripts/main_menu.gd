d4a37f0c24f7400f4aa4ba721df433c35bd2fb89
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2025-12-06 17:47:26 -0500

    navigation works for main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,17 +1,12 @@
-extends Node2D
+extends Control
 
 @onready var startButton := $button_manager/start_button
 
-func _onready() -> void:
+func _ready() -> void:
 	startButton.grab_focus()
 
 func _on_start_button_pressed() -> void:
 	pass # Replace with function body.
-
-
-func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
-
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
@@ -21,3 +16,6 @@ func _on_dev_vault_start_button_pressed() -> void:
 
 func _on_dev_arena_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/test_arena.tscn")
+
+func _on_load_button_pressed() -> void:
+	pass # Replace with function body.
