478ff2039d474fde00dd9d8d84a493f08216c4be
Forrest Storrs <F.Storrs19+sloane@gmail.com>
2025-12-09 16:03:28 -0500

    speaker text works, dialog skipping works, fixed start menu positioning

--- scripts/dialog_system.gd
+++ scripts/dialog_system.gd
@@ -8,20 +8,29 @@ extends Control
 
 var is_dialog_active := false
 var dialog_text_array
+var total_lines = 0
 
 func _ready() -> void:
 	background.visible = false
 	speaker_name.visible = false
+	dialog_text.visible = false
+	print(speaker_name.visible)
 	
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("dialog_next_line") and is_dialog_active:
-		active_line_index += 1
-		play_line_of_dialog()
+		if animation_player.is_playing():
+			animation_player.stop()
+			dialog_text.visible_ratio = 1.0
+		elif active_line_index <= total_lines:
+			active_line_index += 1
+			play_line_of_dialog()
+		else:
+			toggle_dialog()
 
 func _on_item_pickup_play_dialog(dialog_to_play: Variant) -> void:
 	toggle_dialog()
 	dialog_text_array = DialogConstants[dialog_to_play]
-	#var total_lines = DialogConstants.dialog_to_play.length - 1
+	total_lines = DialogConstants[dialog_to_play].size() - 1
 	play_line_of_dialog()
 	
 func play_line_of_dialog() -> void:
@@ -29,6 +38,7 @@ func play_line_of_dialog() -> void:
 	animation_player.play("scroll")
 
 func toggle_dialog() -> void:
+	dialog_text.visible = !dialog_text.visible
 	background.visible = !background.visible
-	speaker_name.visible = !background.visible
+	speaker_name.visible = !speaker_name.visible
 	is_dialog_active = !is_dialog_active
